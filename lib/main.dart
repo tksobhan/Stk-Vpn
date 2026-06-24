58a074c9b0bee6e4dd63c5b2c80bc27a3fdd3104
tksobhan <tksobhan@users.noreply.github.com>
2026-06-24 19:34:57 +0330

    پروژه ساده VPN از صفر

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My VPN',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('VPN اتصال')),
+      body: const Center(
+        child: Text('سلام! اپلیکیشن VPN'),
+      ),
+    );
+  }
+}
